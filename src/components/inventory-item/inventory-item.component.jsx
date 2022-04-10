3dc63692e8f83801af953d4a91b4a833687ba6ad
Lucidreline <casta.ma502@gmail.com>
2022-04-10 02:13:32 -0700

    Major improvements to the inventory list.

--- src/components/inventory-item/inventory-item.component.jsx
+++ src/components/inventory-item/inventory-item.component.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
-import './inventory-item.styles.scss'
+import React, { useState } from "react";
+import "./inventory-item.styles.scss";
 
 const InventoryItem = ({ itemAttributes }) => {
-
-
-    const { category, sku, image, size, fit, fiber } = itemAttributes
-    const [sizeStr] = useState(() => {
-        let str = ''
-        for (const key in size) {
-            if (size[key]) {
-                str = str.concat(`${key}, `)
-            }
-        }
-        return str.slice(0, -2)
+  const { category, sku, image, size, fit, fiber } = itemAttributes;
+  const [sizeStr] = useState(() => {
+    let str = "";
+    for (const key in size) {
+      if (size[key]) {
+        if (key === "twoX") str = str.concat(`xx, `);
+        else if (key === "threeX") str = str.concat(`xxx, `);
+        else str = str.concat(`${key}, `);
+      }
     }
-    )
+    return str.slice(0, -2);
+  });
 
-
-    return (
-        <div className='inventory-item'>
-            <div className="inventory-item-image" style={{
-                backgroundImage: `url("${image}")`,
-            }}></div>
-            <span className="inventory-item-category">{category}</span>
-            <span className="inventory-item-sku">{sku}</span>
-            <span className="inventory-item-size">{sizeStr}</span>
-            <span className="inventory-item-fit">{fit}</span>
-            <span className="inventory-item-fiber">{fiber}</span>
-
-        </div>
-    );
-}
+  return (
+    <tr>
+      <td
+        className="inventory-item-image"
+        style={{
+          backgroundImage: `url("${image}")`,
+        }}
+      ></td>
+      <td>{sku}</td>
+      <td>{category}</td>
+      <td>{sizeStr}</td>
+      <td>{fit}</td>
+      <td>{fiber}</td>
+    </tr>
+  );
+};
 
 export default InventoryItem;
--- src/components/inventory-list/inventory-list.component.jsx
+++ src/components/inventory-list/inventory-list.component.jsx
@@ -1,45 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import { Button } from 'react-bootstrap';
-import InventoryItem from '../inventory-item/inventory-item.component';
+import React, { useState, useEffect } from "react";
+import { Button, Table } from "react-bootstrap";
+import InventoryItem from "../inventory-item/inventory-item.component";
 
 const InventoryList = (props) => {
-    const headerAttributes = {
-        category: 'Category',
-        sku: "SKU",
-        image: '',
-        size: 'Size',
-        fit: 'Fit',
-        fiber: 'Fiber'
-    }
+  const headerAttributes = {
+    category: "Category",
+    sku: "SKU",
+    image: "",
+    size: "Size",
+    fit: "Fit",
+    fiber: "Fiber",
+  };
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [clothing, setClothing] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [clothing, setClothing] = useState([]);
 
-    useEffect(() => {
-        fetch(
-          '/clothing'
-        )
-          .then(res => res.json())
-          .then(response => {
-            setClothing(response.results);
-            setIsLoading(false);
-          })
-          .catch(error => console.log(error));
-      }, []);
+  useEffect(() => {
+    fetch("/clothing")
+      .then((res) => res.json())
+      .then((response) => {
+        setClothing(response.results);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    
+  return (
+    <div className="mid">
+      {isLoading && <p>One Second, I'm still loading...</p>}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>SKU</th>
+            <th>Category</th>
+            <th>Size</th>
+            <th>Fit</th>
+            <th>Fiber</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clothing.map((item) => (
+            <InventoryItem
+              key={item.attributes.sku}
+              itemAttributes={item.attributes}
+            />
+          ))}
+        </tbody>
+      </Table>
 
-    return (
-        <div className='mid'>
-            <InventoryItem itemAttributes={headerAttributes} />
-            {isLoading && <p>One Second, I'm still loading...</p>}
-            {clothing.map((item) => (
-                <InventoryItem key={item.attributes.sku} itemAttributes={item.attributes } />
-            ))}
-
-            <Button>Export Data</Button>
-        </div>
-    );
-}
+      <Button>Export Data</Button>
+    </div>
+  );
+};
 
 export default InventoryList;
