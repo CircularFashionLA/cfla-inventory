f8a24e5ae66d26410d23e2c245cf7ec9e1a8a713
Lucidreline <casta.ma502@gmail.com>
2022-04-11 21:09:03 -0700

    Fixed error where variable was not defined. Don't read last commit hehe

--- src/components/inventory-list/inventory-list.component.jsx
+++ src/components/inventory-list/inventory-list.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CsvDownload from "react-json-to-csv";
+import { Table } from "react-bootstrap";
 import InventoryItem from "../inventory-item/inventory-item.component";
 
 import "./inventory-list.styles.scss";
