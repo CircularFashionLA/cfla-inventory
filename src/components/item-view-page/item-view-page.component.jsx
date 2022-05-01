5036bbbccde9a05ea9e302d38cf76252b6c1eff4
Lucidreline <casta.ma502@gmail.com>
2022-05-01 00:55:04 -0700

    Added measurements to item view page.

--- src/components/item-view-page/item-view-page.component.jsx
+++ src/components/item-view-page/item-view-page.component.jsx
@@ -5,6 +5,8 @@ import {
   stringifySizes,
 } from "../../utils/stringify-attributes";
 
+import "./item-view-page.styles.scss";
+
 const ItemViewPage = ({ item }) => {
   try {
     const {
@@ -41,27 +43,113 @@ const ItemViewPage = ({ item }) => {
 
     return (
       <>
-        <ul>
-          <li>
-            <b>Category </b>
-            {category}
-            {subcategory && ` - ${subcategory}`}
-          </li>
-          <li>
-            <b>Color </b> {stringifyColors(colors)}
-          </li>
-          <li>
-            <b>Size </b> {stringifySizes(size)} - {stringifyFit(fit)}
-          </li>
-          <li>
-            <b>Fiber </b> {fiber}
-          </li>
-          <li>
-            <b>Other </b> {sleeveless && "Sleeveless"}
-            {offShoulder && ", Off Shoulder"}
-            {stretchy && ", Stretchy"} {adjustable && ", Adjustable"}
-          </li>
-        </ul>
+        <>
+          <h1>
+            {productName} ({sku})
+          </h1>
+        </>
+        <div
+          className="item-image"
+          style={{
+            backgroundImage: `url("${image}")`,
+          }}
+        ></div>
+        <div className="item-data">
+          <div className="item-data-attributes">
+            <h2>Attributes</h2>
+            <ul>
+              <li>
+                <b>Category </b>
+                {category}
+                {subcategory && ` - ${subcategory}`}
+              </li>
+              <li>
+                <b>Color </b> {stringifyColors(colors)}
+              </li>
+              <li>
+                <b>Size </b> {stringifySizes(size)} - {stringifyFit(fit)}
+              </li>
+              <li>
+                <b>Fiber </b> {fiber}
+              </li>
+              <li>
+                <b>Other </b> {sleeveless && "Sleeveless"}
+                {offShoulder && ", Off Shoulder"}
+                {stretchy && ", Stretchy"} {adjustable && ", Adjustable"}
+              </li>
+            </ul>
+          </div>
+          <div className="item-data-measurements">
+            <h2>Measurements</h2>
+            <ul>
+              {shoulderWidth && (
+                <li>
+                  <b>Shoulder Width</b> {shoulderWidth}"
+                </li>
+              )}
+              {chestWidth && (
+                <li>
+                  <b>Chest Wisdth</b> {chestWidth}"
+                </li>
+              )}
+              {bicepWidth && (
+                <li>
+                  <b>Bicep Width</b> {bicepWidth}"
+                </li>
+              )}
+              {sleeveLength && (
+                <li>
+                  <b>Sleeve Length</b> {sleeveLength}"
+                </li>
+              )}
+              {waistWidth && (
+                <li>
+                  <b>Waist Width</b> {waistWidth}"
+                </li>
+              )}
+              {bottomHemSweep && (
+                <li>
+                  <b>Bottom Hem Sweep</b> {bottomHemSweep}"
+                </li>
+              )}
+              {rise && (
+                <li>
+                  <b>Rise</b> {rise}"
+                </li>
+              )}
+              {hipWidth && (
+                <li>
+                  <b>Hip Width</b> {hipWidth}"
+                </li>
+              )}
+              {topOfChestToCrotch && (
+                <li>
+                  <b>Top of Chest to Crotch</b> {topOfChestToCrotch}"
+                </li>
+              )}
+              {thighWidth && (
+                <li>
+                  <b>Thigh Width</b> {thighWidth}"
+                </li>
+              )}
+              {inseam && (
+                <li>
+                  <b>Inseam</b> {inseam}"
+                </li>
+              )}
+              {outseam && (
+                <li>
+                  <b>Outseam</b> {outseam}"
+                </li>
+              )}
+              {garmetLength && (
+                <li>
+                  <b>Garmet Length</b> {garmetLength}"
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
       </>
     );
   } catch (error) {
