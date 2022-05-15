cef35b58e195baf01dfa2e8cdab3cc2ce4a9e226
Lucidreline <casta.ma502@gmail.com>
2022-05-15 01:51:13 -0700

    Styled form and Nav.

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "./nav.styles.scss";
 
 const Nav = (props) => {
+  const getUrl = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const location = useLocation();
+    console.log(location.pathname);
+  };
+
   return (
-    <nav className="mid nav">
-      <Link className="nav-links" to="/">
-        Inventory
-      </Link>
-      <Link className="nav-links" to="/form">
-        Form
-      </Link>
+    <nav className="nav">
+      <div className="postion">
+        <div className="nav-link-container">
+          <Link className="nav-links" onClick={() => getUrl()} to="/">
+            Inventory
+          </Link>
+        </div>
+      </div>
+      <div className="nav-link-container">
+        <Link className="nav-links" onClick={() => getUrl()} to="/form">
+          Form
+        </Link>
+      </div>
     </nav>
   );
 };
