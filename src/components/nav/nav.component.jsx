be2f2df13c0bb0c857662eec8c13f34cd71d9c6c
Lucidreline <casta.ma502@gmail.com>
2022-05-16 10:21:42 -0700

    Active links are displayed differently than unactive.

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import "./nav.styles.scss";
 
 const Nav = (props) => {
-  const getUrl = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const location = useLocation();
-    console.log(location.pathname);
-  };
+  const location = useLocation();
+  const [url, setUrl] = useState(location.pathname);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        setUrl("/");
+        break;
+      case "/form":
+        setUrl("/form");
+        break;
+      default:
+    }
+  }, [location]);
 
   return (
     <nav className="nav">
       <div className="postion">
-        <div className="nav-link-container">
-          <Link className="nav-links" onClick={() => getUrl()} to="/">
+        <div className={`nav-link-container ${url === "/" ? "active" : ""}`}>
+          <Link className={`nav-links`} to="/">
             Inventory
           </Link>
         </div>
       </div>
-      <div className="nav-link-container">
-        <Link className="nav-links" onClick={() => getUrl()} to="/form">
+      <div className={`nav-link-container ${url === "/form" ? "active" : ""}`}>
+        <Link className={`nav-links`} to="/form">
           Form
         </Link>
       </div>
